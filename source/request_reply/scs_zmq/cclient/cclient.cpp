6281b67d99df4acb564c4602092e0abae9943eec
diaoguangqiang <diaoguangqiang@126.com>
2017-04-14 13:15:27 +0800

    修复发包长度

--- source/request_reply/scs_zmq/cclient/cclient.cpp
+++ source/request_reply/scs_zmq/cclient/cclient.cpp
@@ -6,9 +6,6 @@
 #include "zmq_utils.h"            //Zeromq 函数的导入在这里帮我们实现了
 #pragma comment(lib,"libzmq-v100-mt-gd-4_0_4.lib")
 
-#define FLAG 1
-//0阻塞,1费阻塞
-
 int main( int argc, char** argv )
 {
 	std::cout << "zmq client, connect: 6666" <<std::endl;
@@ -27,45 +24,34 @@ int main( int argc, char** argv )
 
 	int times = 0;
 
-	int msg_len = 25;
+	int msg_len = 15;
 
 	std::string msg_str = "Z26_PSC_JLB2_2";
-	//printf("msg_len:%d\n\n", msg_str.length());
+	printf("msg_len:%d\n\n", msg_str.length());
 
 	while(true)
 	{
 		//发送部分
 		zmq_msg_t send_msg;
 		zmq_msg_init_size(&send_msg, msg_len );
-		char buf[]={"Z26_PSC_JLB2_2"};
-		memcpy(zmq_msg_data(&send_msg), buf, msg_len );
-		int size = zmq_msg_size(&send_msg);
-		printf("client >>> 消息大小:%d\n", size);
-		//发出消息
-		zmq_msg_send(&send_msg, z_socket, FLAG);
-		printf("client >>> msg:\t%s\n", (char*)zmq_msg_data(&send_msg));
-		//std::cout<<(char*)zmq_msg_data(&send_msg)<<std::endl;
+		memcpy(zmq_msg_data(&send_msg), "Z26_PSC_JLB2_2", msg_len );
+		zmq_msg_send(&send_msg,z_socket,0);
+		printf("client >>> msg:\t");
+		std::cout<<(char*)zmq_msg_data(&send_msg)<<std::endl;
 		zmq_msg_close(&send_msg);
 
 		//接受部分
 		zmq_msg_t recv_msg;
 		zmq_msg_init(&recv_msg);
 		//0表示非阻塞
-		int recv = zmq_msg_recv(&recv_msg, z_socket, FLAG); 
-		size_t recv_size = zmq_msg_size(&recv_msg);
-		if ( recv_size > 0 )
-		{
-			printf("client <<< msg:\t%s\n", (char*)zmq_msg_data(&recv_msg) );
-		}else{
-			printf("client <<< no msg recv! line[%d]\n", __LINE__ );
-		}
-		
-		//std::cout<<(char*)zmq_msg_data(&recv_msg)<<std::endl;
+		zmq_msg_recv(&recv_msg,z_socket,0);                    
+		printf("client <<< msg:\t");
+		std::cout<<(char*)zmq_msg_data(&recv_msg)<<std::endl;
 		zmq_msg_close(&recv_msg);
 
 		times++;
 
-		Sleep(3000);
+		Sleep(300);
 
 		printf("\n");
 	}
--- source/request_reply/scs_zmq/cserver/cserver.cpp
+++ source/request_reply/scs_zmq/cserver/cserver.cpp
@@ -6,10 +6,6 @@
 
 #pragma comment(lib,"libzmq-v100-mt-gd-4_0_4.lib")
 
-#define FLAG 1
-//0阻塞,1非阻塞
-
-
 int main(int argc, char** argv)
 {	
 	std::cout << "zmq server, listen: 6666" <<std::endl;
@@ -32,35 +28,23 @@ int main(int argc, char** argv)
 		//接受部分
 		zmq_msg_t recv_msg;
 		zmq_msg_init(&recv_msg);
-		zmq_msg_recv(&recv_msg,z_socket, FLAG);
-		size_t recv_size = zmq_msg_size(&recv_msg);
-		if ( recv_size > 0 )
-		{
-			printf("server <<< size: %d, %s\n", /*recvn++*/zmq_msg_size(&recv_msg), (char*)zmq_msg_data(&recv_msg));
-		}else{
-			printf("server <<< no msg recv! line[%d]\n", __LINE__);
-		}
+		zmq_msg_recv(&recv_msg,z_socket,0);    
 		 //0表示非阻塞
-		//std::cout<<"server <<< msg: "<<recvn++<<" ";
-		//std::cout<<(char*)zmq_msg_data(&recv_msg)<<std::endl;
+		std::cout<<"server <<< msg: "<<recvn++<<" ";
+		std::cout<<(char*)zmq_msg_data(&recv_msg)<<std::endl;
 		zmq_msg_close(&recv_msg);
 
 		//发送部分
 		zmq_msg_t send_msg;
-		zmq_msg_init_size(&send_msg, msg_len);
+		zmq_msg_init_size(&send_msg,msg_len);
 		//memcpy(zmq_msg_data(&send_msg), "{\"groupno\":\"451\",\"id\":\"2\",\"name\":\"Z26_PSC_JLB2_2\",\"type\":\"JLB2\",\"lable\":\"lable\",\"loaction\":\"location\",\"timestamp\":\"0\",\"quality\":\"0\",\"point\":[{\"tag\":\"ABDY\",\"offset\":\"790\",\"type\":\"1\",\"value\":\"0\"},{\"tag\":\"AXDL\",\"offset\":\"791\",\"type\":\"1\",\"value\":\"0\"},{\"tag\":\"BCDY\",\"offset\":\"792\",\"type\":\"1\",\"value\":\"0\"},{\"tag\":\"BXDL\",\"offset\":\"793\",\"type\":\"1\",\"value\":\"0\"},{\"tag\":\"CADY\",\"offset\":\"794\",\"type\":\"1\",\"value\":\"0\"},{\"tag\":\"CXDL\",\"offset\":\"795\",\"type\":\"1\",\"value\":\"0\"},{\"tag\":\"DYPL\",\"offset\":\"796\",\"type\":\"1\",\"value\":\"0\"},{\"tag\":\"GLYS\",\"offset\":\"797\",\"type\":\"1\",\"value\":\"0\"},{\"tag\":\"WGDD\",\"offset\":\"798\",\"type\":\"1\",\"value\":\"0\"},{\"tag\":\"WGGL\",\"offset\":\"799\",\"type\":\"1\",\"value\":\"0\"},{\"tag\":\"YGDD\",\"offset\":\"800\",\"type\":\"1\",\"value\":\"0\"},{\"tag\":\"YGGL\",\"offset\":\"801\",\"type\":\"1\",\"value\":\"0\"}]}", msg_len);
 		memcpy(zmq_msg_data(&send_msg), msg_buf, msg_len);
-		zmq_sendmsg(z_socket,&send_msg, FLAG);
-		printf("server >>> send success! msg[%s] line[%d]\n", msg_buf, __LINE__);
-		//std::cout<<"server >>> msg: success! len "<< msg_len <<std::endl;
+		zmq_sendmsg(z_socket,&send_msg,0);
+		std::cout<<"server >>> msg: success! len "<< msg_len;
 		//std::cout<<(char*)zmq_msg_data(&send_msg)<<std::endl;
 		zmq_msg_close(&send_msg);
 
-		printf("server running ....\n\n");
-
-		//50毫秒
-		Sleep(50);
-		
+		printf("\n\n");
 	}
 	zmq_close(z_socket);
 	zmq_term(context);
